479772f8130d5636789fe789de8a3b2d8d642a38
Daniel Barlow <dan@telent.net>
2018-08-13 00:40:10 +0100

    displays text/plain messages

--- src/epsilon/client.cljs
+++ src/epsilon/client.cljs
@@ -5,12 +5,9 @@
             [re-frame.core :as rf]
             [clojure.string :as str]))
 
-;; A detailed walk-through of this source code is provided in the docs:
-;; https://github.com/Day8/re-frame/blob/master/docs/CodeWalkthrough.md
-
 ;; -- Domino 1 - Event Dispatch -----------------------------------------------
 
-(defn dispatch-timer-event
+#_(defn dispatch-timer-event
   []
   (let [now (js/Date.)]
     (rf/dispatch [:timer now])))  ;; <-- dispatch used
@@ -18,7 +15,7 @@
 ;; Call the dispatching function every second.
 ;; `defonce` is like `def` but it ensures only one instance is ever
 ;; created in the face of figwheel hot-reloading of this file.
-(defonce do-timer (js/setInterval dispatch-timer-event 1000))
+#_ (defonce do-timer (js/setInterval dispatch-timer-event 1000))
 
 
 ;; -- Domino 2 - Event Handlers -----------------------------------------------
@@ -43,6 +40,12 @@
      :response-format (ajax/json-response-format {:keywords? true})
      :on-success      [:search-result]
      :on-failure      [:search-error]}}))
+
+(rf/reg-event-db
+ :show-completions
+ (fn [db [_ val]]
+   ;; XXX dispatch the autocomplete xhr req here
+   (assoc db :show-completions val)))
 
 (rf/reg-event-db
  :search-result
@@ -79,18 +82,17 @@
    (assoc db :error error)))
 
 
-(rf/reg-event-db                 ;; usage:  (dispatch [:timer a-js-Date])
-  :timer                         ;; every second an event of this kind will be dispatched
-  (fn [db [_ new-time]]          ;; note how the 2nd parameter is destructured to obtain the data value
-    (assoc db :time new-time)))  ;; compute and return the new application state
-
-
 ;; -- Domino 4 - Query  -------------------------------------------------------
 
 (rf/reg-sub
   :search-term
   (fn [db _]
     (:search-term db)))
+
+(rf/reg-sub
+  :show-completions
+  (fn [db _]
+    (:show-completions db)))
 
 (rf/reg-sub
   :search-result
@@ -110,33 +112,40 @@
   [:div.search-term-input
    {:placeholder "Search messages"}
    [:input {:type "text"
+            :auto-complete "off"
             :value @(rf/subscribe [:search-term])
             :on-change
             #(rf/dispatch [:search-requested (-> % .-target .-value)])
             }]])
 
+
 (defn tags
   []
-  [:ul.taglist
-   (map (fn [tag]
-          [:li
-           {:on-click #(rf/dispatch [:search-requested (str "tag:" tag)])}
-           tag])
-        ["inbox" "new" "spam"
-         "attachment"
-         "deleted"
-         "draft"
-         "fb"
-         "ham"
-         "inbox"
-         "new"
-         "refiled"
-         "replied"
-         "report"
-         "signed"
-         "spam"
-         "trash"
-         "unread"])])
+  [:div.taglist
+   [:ul
+    (map (fn [tag]
+           [:li
+            {:key tag
+             :on-click
+             (fn [e]
+               (rf/dispatch [:show-completions false])
+               (rf/dispatch [:search-requested (str "tag:" tag)]))
+             }
+            tag])
+         ["inbox"
+          "attachment"
+          "deleted"
+          "draft"
+          "fb"
+          "ham"
+          "new"
+          "refiled"
+          "replied"
+          "report"
+          "signed"
+          "spam"
+          "trash"
+          "unread"])]])
 
 
 (defn search-result
@@ -154,27 +163,61 @@
               ])
            rs)]]))
 
+(defmulti render-message-part (fn [m] (:content-type m)))
+
+(defmethod render-message-part "multipart/alternative" [m]
+  (let [c (:content m)
+        plain (first (filter #(= (:content-type %) "text/plain") c))]
+    (render-message-part plain)))
+
+(defmethod render-message-part "multipart/mixed" [m]
+  (let [c (:content m)]
+    (map render-message-part c)))
+
+(defmethod render-message-part "text/plain" [m]
+  [:div {:key (:id m)} [:pre (:content m)]])
+
+(defmethod render-message-part :default [m]
+  ;; XXX apparently the DOMParser is the best way to do html
+  [:div {:key (:id m)} [:pre "mime type " (:content-type m) " not supported"]])
+
+(defn render-message [m]
+  [:div.message {:key (:id m)}
+   [:div.headers
+    (map (fn [[k v]] [:div.header {:key k} [:span.name (name k) ":"] v]) (:headers m))]
+   [:div (map (fn [tag] [:span.tag {:key tag} tag]) (:tags m))]
+   [:div.message-body (map render-message-part (:body m))]])
+
+
+(defn render-thread [[frst & replies]]
+  [:div.thread {:key (str "th:" (:id frst))}
+   (render-message frst)
+   (map render-thread (first replies))])
+
 (defn thread-pane
   []
-  (let [m (first (first (first @(rf/subscribe [:thread]))))]
-    (println m)
-    [:div.message
-     [:div.headers
-      (map (fn [[k v]] [:div {:key k} [:b (name k)] v]) (:headers m))]
-     [:div (map (fn [tag] [:span.tag {:key tag} tag]) (:tags m))]
-     [:div (pr-str (:body m))]]))
+  (let [m (first (first  @(rf/subscribe [:thread])))]
+    (render-thread m)))
 
 
 (defn ui
   []
   [:div
-   [:div {:id "search"}
-    [search-term-input]
-    [tags]]
-   [:div {:id "threads"}
-    [search-result]]
-   [:div {:id "thread"}
-    [thread-pane]]])
+   (if @(rf/subscribe [:thread])
+     [:div
+      [:div {:on-click #(rf/dispatch [:thread-retrieved nil])}
+       "Back"
+       ]
+      [:div.thread
+       [thread-pane]]]
+     [:div
+      [:div.search
+       {:on-focus #(rf/dispatch [:show-completions true])}
+       [search-term-input]
+       (if (or false  @(rf/subscribe [:show-completions])) [tags])]
+      [:div {:id "threads"}
+       [search-result]]])])
+
 
 ;; -- Entry Point -------------------------------------------------------------
 
