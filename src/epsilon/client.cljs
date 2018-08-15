32b62b400747a9bc36fcdb76389363e68738c955
Daniel Barlow <dan@telent.net>
2018-08-15 01:07:46 +0100

    dead code delenda est

--- src/epsilon/client.cljs
+++ src/epsilon/client.cljs
@@ -8,16 +8,6 @@
 
 ;; -- Domino 1 - Event Dispatch -----------------------------------------------
 
-#_(defn dispatch-timer-event
-  []
-  (let [now (js/Date.)]
-    (rf/dispatch [:timer now])))  ;; <-- dispatch used
-
-;; Call the dispatching function every second.
-;; `defonce` is like `def` but it ensures only one instance is ever
-;; created in the face of figwheel hot-reloading of this file.
-#_ (defonce do-timer (js/setInterval dispatch-timer-event 1000))
-
 
 ;; -- Domino 2 - Event Handlers -----------------------------------------------
 
