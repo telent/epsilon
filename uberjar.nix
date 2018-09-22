3d3658220d46b05030856e508993356b5dde716d
Daniel Barlow <dan@telent.net>
2018-09-22 23:39:16 +0100

    attempt ci build

---
+++ uberjar.nix
@@ -0,0 +1,10 @@
+with import <nixpkgs> {};
+let epsilon = callPackage ./default.nix {};
+in epsilon.overrideAttrs(o: {
+  nativeBuildInputs = [ pkgs.unzip ] ++ o.nativeBuildInputs;
+  installPhase = ''
+    for i in `echo $CLASSPATH | sed 's/:/ /g'`; do unzip -o  $i -d target ;done
+    mkdir -p $out
+    jar cef epsilon.server $out/epsilon-standalone.jar -C target  .
+  '';
+})
